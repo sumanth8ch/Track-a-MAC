842902f8da20a418c7b5453e94066d9fa4280958
haritha11d <43556074+haritha11d@users.noreply.github.com>
2019-10-27 22:49:32 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+   class MyDB extends SQLite3 {
+      function __construct() {
+         $this->open('ourdb.db');
+      }
+   }
+   $db = new MyDB();
+   $trapdata =<<<EOF
+      CREATE TABLE IF NOT EXISTS finalproject(IPADDRESS VARCHAR NOT NULL,VLAN VARCHAR NOT NULL,PORT VARCHAR,MACS VARCHAR);
+EOF;
+$result = $db->exec($trapdata);
+   if(!$result){
+       $db->lastErrorMsg();
+   }
+$manager =<<<EOF
+      CREATE TABLE IF NOT EXISTS manager(ip VARCHAR NOT NULL,port VARCHAR NOT NULL,community STRING NOT NULL,version VARCHAR NOT NULL,firstprob VARCHAR NULL,lastprob VARCHAR NULL);
+EOF;
+$result = $db->exec($manager);
+   if(!$result){
+       $db->lastErrorMsg();
+   }
+   ?>
