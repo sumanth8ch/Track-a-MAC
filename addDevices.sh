da82d50e92a73754e2de5920177b81bee918a0fa
mbut17 <madaraboina.deepak94@gmail.com>
2020-01-05 21:49:56 +0100

    addDevices.sh added

---
+++ addDevices.sh
@@ -0,0 +1,8 @@
+curl "localhost:8080/config.php"
+curl "localhost:8080/addDevice.php?ip=192.168.184.21&port=161&community=public&version=2"
+curl "localhost:8080/addDevice.php?ip=192.168.184.23&port=161&community=public&version=2"
+curl "localhost:8080/addDevice.php?ip=192.168.184.98&port=161&community=public&version=2"
+curl "localhost:8080/addDevice.php?ip=194.47.157.51&port=1024&community=public&version=2"
+curl "localhost:8080/addDevice.php?ip=194.47.157.51&port=1025&community=public&version=2"
+curl "localhost:8080/addDevice.php?ip=194.47.157.51&port=1026&community=public&version=2"
+curl "localhost:8080/addDevice.php?ip=194.47.157.51&port=1027&community=public&version=2"
